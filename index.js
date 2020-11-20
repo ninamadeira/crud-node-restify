4648abdd0ee7dca198c3e326aec3fb440f2aa3ab
Nina Rosa Madeira <nina.madeira@gmail.com>
2020-11-19 21:30:58 -0300

    Project crud-node-restify created

---
+++ index.js
@@ -0,0 +1,89 @@
+const restify = require('restify');
+const errs = require('restify-errors');
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'senha',
+      database : 'db'
+    }
+  });
+
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+// rotas REST
+
+server.get('/', (req, res, next) => {
+    
+    knex('contato').then((dados) => {
+        res.send(dados);
+    }, next)
+    
+});
+
+server.post('/create', (req, res, next) => {
+    
+    knex('contato')
+        .insert(req.body)
+        .then((dados) => {
+            res.send(dados);
+        }, next)
+    
+});
+
+server.get('/show/:id', (req, res, next) => {
+    
+    const { id } = req.params;
+
+    knex('contato')
+        .where('id', id)
+        .first()
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+        }, next)
+        
+});
+
+server.put('/update/:id', (req, res, next) => {
+    
+    const { id } = req.params;
+
+    knex('contato')
+        .where('id', id)
+        .update(req.body)
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send('dados atualizados');
+        }, next)
+        
+});
+
+server.del('/delete/:id', (req, res, next) => {
+    
+    const { id } = req.params;
+
+    knex('contato')
+        .where('id', id)
+        .delete()
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send('dados excluidos');
+        }, next)
+        
+});
